fc6dae7e2c6c9c7581e43660d63a888114cb220f
SergeyBordulev <bordulev@yahoo.com>
2021-02-04 16:52:49 +0700

    Initial commit

---
+++ src/AverageValue.java
@@ -0,0 +1,8 @@
+public class AverageValue {
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 20;
+        int result = (a + b) / 2;
+        System.out.println(result);
+    }
+}
